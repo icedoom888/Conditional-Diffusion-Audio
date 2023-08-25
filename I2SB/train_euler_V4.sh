2e2e75e02b9e1236556a35385f5094da183a7b4e
MrBirrd <mathias-vogel@hotmail.ch>
2023-08-25 14:48:58 +0200

    i2sb v4 euler config

---
+++ I2SB/train_euler_V4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH -n 10
+#SBATCH --cpus-per-task=2
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --job-name=V4
+#SBATCH --output=V4.out
+#SBATCH --error=V4.err
+#SBATCH --gpus=8
+#SBATCH --gres=gpumem:20G
+#SBATCH -A ls_drzrh
+
+python train.py --ckpt V4 --n-gpu-per-node 8 --batch-size 64 --microbatch 4 --beta-max 1.0 --num-itr 100000 --model_conf_path configs/V4.yaml --log-writer wandb --ot-ode --add-x1-noise
